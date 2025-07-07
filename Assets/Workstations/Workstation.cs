c6df783ce5056ff3504fac5dc47003b1a3024f99
Kyra <klz245@nyu.edu>
2025-07-07 13:42:03 -0700

    added basic timer to workstation

--- Assets/Workstations/Workstation.cs
+++ Assets/Workstations/Workstation.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Workstation : MonoBehaviour
 {
     [SerializeField] float interactionTimeRequired;
     [SerializeField] float requiredPlayerRadius;
+    [SerializeField] Slider timer;
 
     Vector2 pos; //saves us from calling to the C++ layer and/or converting Vector3 to Vector2 each frame
     float totalTimeInteracted = 0;
@@ -16,17 +18,26 @@ public class Workstation : MonoBehaviour
     }
 
     private void Update() {
-        //do not interact if player is out of range
-        if ((pos - Player.Position).sqrMagnitude > requiredPlayerRadius * requiredPlayerRadius)
-            return;
-
-        if (Input.GetButton("Interact")) {
-            totalTimeInteracted += Time.deltaTime;
-            Debug.Log($"interacted for {totalTimeInteracted} seconds");
+        
+        if ((pos - Player.Position).sqrMagnitude <= requiredPlayerRadius * requiredPlayerRadius) {
+            if (Input.GetButton("Interact")) {
+                totalTimeInteracted += Time.deltaTime;
+                Debug.Log($"interacted for {totalTimeInteracted} seconds");
+            }
         }
+
+        UpdateTimerVisual();
+    }
+
+    private void UpdateTimerVisual() {
+        float value = totalTimeInteracted / interactionTimeRequired;
+        timer.value = value;
+        timer.gameObject.SetActive(value >= 0.01f); //if timer is close to 0 (with some margin of error), hide timer
     }
 
     private void Reset() {
         totalTimeInteracted = 0;
     }
+
+
 }
